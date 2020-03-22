dfbbf495053c5d55f77ef844811498de5c37eeef
rockts <rockts@sina.com>
2020-03-23 01:41:05 +0800

    用 Mail.send 发送分享的文章邮件

--- app/Controllers/Http/ShareController.js
+++ app/Controllers/Http/ShareController.js
@@ -12,8 +12,20 @@ class ShareController {
        const post = await Post.find(params.id)
        const author = await post.user().fetch()
 
+       // await Mail.raw(`<div><h1>${ post.title }</h1>${ post.content }</div>`, (message) => {
+       //   message
+       //    .to(user.email)
+       //    .from('dev-demo@hola.lekee.cc')
+       //    .subject(`《${ post.title }》 - ${ author.username }`)
+       // })
 
-       await Mail.raw(`<div><h1>${ post.title }</h1>${ post.content }</div>`, (message) => {
+       await Mail.send('email.share_post',
+       {
+         host: request.header('host'),
+         post: post.toJSON(),
+         user: user.toJSON()
+       },
+       (message) => {
          message
           .to(user.email)
           .from('dev-demo@hola.lekee.cc')
