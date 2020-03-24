c7b791d557f2aa9a5c3f577007943a1032b4f8f0
rockts <rockts@sina.com>
2020-03-24 20:17:11 +0800

    自动化同化浏览器变化（使用 Browser-Sync 创建 gulp 任务）

--- app/Controllers/Http/AntlDemoController.js
+++ app/Controllers/Http/AntlDemoController.js
@@ -4,7 +4,8 @@ class AntlDemoController {
   async demo ({ view, antl, locale }) {
     return view.render('demo.antl', {
       greeting: antl.formatMessage('demo.greeting', { name: 'gaopeng' }),
-      locale
+      locale,
+      message: 'hello'
     })
   }
 }
